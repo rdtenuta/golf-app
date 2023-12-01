805e3547dafe71abb74cff287de4dede25e201e6
Ralph Tenuta <toritenuta@Victorias-MacBook-Air.local>
2023-11-30 21:40:21 -0600

    added xcode base

---
+++ xcode/golfapp/golfapp/golfappApp.swift
@@ -0,0 +1,17 @@
+//
+//  golfappApp.swift
+//  golfapp
+//
+//  Created by Ralph Tenuta on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct golfappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
