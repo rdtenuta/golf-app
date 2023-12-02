fad99d3f8271580969b50599259061cea14b60dc
Ralph Tenuta <toritenuta@Victorias-MacBook-Air.local>
2023-12-01 21:57:42 -0600

    init xcode app

---
+++ app/app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  app
+//
+//  Created by Ralph Tenuta on 12/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app/app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Ralph Tenuta on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
